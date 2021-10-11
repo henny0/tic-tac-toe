a5f7c37eeb31b81804202a5844af4cf62aa15476
henny0 <hubert.borowski02@gmail.com>
2021-10-11 12:19:14 +0000

    tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -1,64 +1,66 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-class Square extends React.Component {
+import ReactDOM, { render } from "react-dom";
+import "./index.css"
+class Square extends React.Component{
     constructor(props){
         super(props);
     }
-
-    render() {
-        return(
-            <button onClick={() => {
-                this.props.onClick();
-            }}
-            >
+render(){
+    return (
+        <button
+        onClick={() => {
+            this.props.onClick();
+        }}
+        >
             {this.props.value}
-            </button>
-        );   
-    }
+        </button>
+    );
 }
-class Board extends React.Component {
+}
+class Board extends React.Component{
     constructor(props){
-        super(props);
-    }
-    render() {
-        return this.props.squares.map((arrayElement, index) => {
-        const arrowSquareFunction = () => this.props.handleSquareClick(index);
-            return (
-                <Square key={index} onClick={arrowSquareFunction} value={arrayElement}
-                />
-            );
-        });
-   }
+    super(props);
 }
+
+render(){
+    return this.props.squares.map((arrayElement, index) => {
+        const arrowSquareFunction = () => this.props.handleSquareClick(index);
+        return (
+            <Square
+            key={index}
+            onClick={arrowSquareFunction}
+            value={arrayElement}
+            />
+        );
+    });
+    }
+}
+
 
 class Game extends React.Component {
     constructor(props){
-        super(props);
-
-
-        this.state={
-            squares: Array(9).fill(null),
-            nextSymbol: "X",
+    super(props);
+    this.state = {
+        squares: Array(9).fill(null),
+        nextSymbol: "O"
         };
-
-        this.handleSquareClick= this.handleSquareClick.bind(this);
+        this.handleSquareClick = this.handleSquareClick.bind(this);
     }
-
-    handleSquareClick(index){
-        console.log(`User click ${index}`);
-        const stateSquares = this.state.nextSymbol;
-        const nextSymbol = this.state.nextSymbol === "X" ? "O" : "X";
-        this.setState({squares : stateSquares, nextSymbol: nextSymbol});
-    }
-
-    render() {
-        return(
-            <Board squares={this.state.squares}
-            handleSquareClick={this.handleSquareClick}
-            />
-        );
-    }
+handleSquareClick(index){
+    const stateSquares = this.state.squares;
+    stateSquares[index] = this.state.nextSymbol;
+    const nextSymbol = this.state.nextSymbol === "X" ? "O" : "X";
+    this.setState({ squares: stateSquares, nextSymbol: nextSymbol});
 }
+render(){
+return (
+    <Board
+    squares={this.state.squares}
+    handleSquareClick={this.handleSquareClick}
+    />
+);
+}
+}
+
 
 ReactDOM.render(<Game />, document.getElementById("root"));
